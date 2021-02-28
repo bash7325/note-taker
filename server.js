657d3cbda810c9e0f4488d22a9bc21c8d329c6b7
Brandon Ashby <bash7325@gmail.com>
2021-02-28 12:47:58 -0700

    route not working

--- server.js
+++ server.js
@@ -2,10 +2,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 //PORT
 const PORT = process.env.PORT || 3000;
+
+//express middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 //Start Server
 app.listen(PORT, () => {
     console.log("server is listening on port:" + PORT)
 });
+
+//routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index/html"))
+    console.log("index")
+})
