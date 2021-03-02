8c37cd0533402fd44ddac6dc09e2c5f5ae3aff52
Brandon Ashby <bash7325@gmail.com>
2021-03-01 19:58:22 -0700

    added some notes, minor formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,44 @@
-const { v4: uuidv4 } = require('uuid');
-const fs = require ("fs");
+//unique ID package and filesystem package
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 module.exports = (app) => {
-  
-  app.get('/api/notes', (req, res) => {
-    fs.readFile("db/db.json", "utf8", function(error,data) {
+  //GET requests.  Read db.json file then return the saved notes as JSON
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf8", function (error, data) {
       res.json(JSON.parse(data));
     });
-    
   });
-
-    app.post("/api/notes", (req, res) => {
+  //POST requests.  saves new notes by adding to the db.json file and assigns a unique id using uuid package
+  app.post("/api/notes", (req, res) => {
     var newNote = req.body;
     newNote.id = uuidv4();
-      fs.readFile("db/db.json", "utf8", (error,data) => {
-        var data = JSON.parse(data);
-        data.push(newNote);
-        fs.writeFile("db/db.json", JSON.stringify(data), (error) => {
-          if (error)
-           throw error;
-           console.log("Note Written");
-        })
+    fs.readFile("db/db.json", "utf8", (error, data) => {
+      var data = JSON.parse(data);
+      data.push(newNote);
+      fs.writeFile("db/db.json", JSON.stringify(data), (error) => {
+        if (error) throw error;
+        console.log("Note Written");
       });
-      res.json(newNote);
-
     });
-    app.delete("/api/notes/:id", (req, res) => {
-      fs.readFile("db/db.json", "utf8", (error, data) => {
-        let noteId = req.params.id;
-        let noteData = JSON.parse(data);
-        noteData = noteData.filter(function(note) {
-            if (noteId != note.id) {
-              return true;
-            } else {
-              return false;
-            };
-        }); 
-        fs.writeFile("db/db.json", JSON.stringify(noteData), (error) => {
-          if (error)
-          throw error;
-          res.end(console.log("Note Deleted"));
-        })
+    res.json(newNote);
+  });
+  //reads all the notes from db.json, removes notes that have the given id then rewrites back to db.json
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("db/db.json", "utf8", (error, data) => {
+      let noteId = req.params.id;
+      let noteData = JSON.parse(data);
+      noteData = noteData.filter(function (note) {
+        if (noteId != note.id) {
+          return true;
+        } else {
+          return false;
+        }
       });
-
+      fs.writeFile("db/db.json", JSON.stringify(noteData), (error) => {
+        if (error) throw error;
+        res.end(console.log("Note Deleted"));
+      });
     });
+  });
 };
--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
 //Start Server
 app.listen(PORT, () => {
   console.log("server is listening on port:" + PORT);
